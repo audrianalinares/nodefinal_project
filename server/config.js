56e75f3857975f1f311186bf6125cf77d5139b64
audrianalinares <audriana_linares@yahoo.com>
2020-08-12 00:25:25 -0500

    config

---
+++ server/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoUri : "mongodb+srv://audrilinares:<linares92>@nodeforum.6hlms.mongodb.net/test"
+}
--- server/nodefinal_project.js
+++ server/nodefinal_project.js
@@ -1,4 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const {connect, connection} = require("mongoose");
 
+const config = require('./config');
+
+connect(config.mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+connection.on('connected', () => console.log('connected to MongoDB'));
+const app = express();
+app.use(bodyParser.json());
