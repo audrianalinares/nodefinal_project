eb6e13e551db8bb1574d0b8af6ba920a502cffa7
audrianalinares <audriana_linares@yahoo.com>
2020-08-12 22:29:38 -0500

    code

--- server/nodefinal_project.js
+++ server/nodefinal_project.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const {connect, connection} = require("mongoose");
-
+const cors = require("cors");
+const path = require("path");
 const config = require('./config');
 
 connect(config.mongoUri, {
@@ -11,3 +12,13 @@ connect(config.mongoUri, {
 connection.on('connected', () => console.log('connected to MongoDB'));
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
+
+app.use(express.static(path,join(__dirname,"..", "client/build")));
+
+app.get('*',(req,res) =>{
+    res.sendFile(path.join(__dirname,"..","client/build/index.html"));
+});
+
+const PORT = 5005;
+app.listen(PORT.() => console.log(`server running on port ${PORT}`));
