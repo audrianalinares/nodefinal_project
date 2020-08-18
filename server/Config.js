1d8211daae7be19f7140c72ce5491fb722924c97
audrianalinares <audriana_linares@yahoo.com>
2020-08-18 00:15:07 -0500

    having trouble connecting

--- server/Config.js
+++ server/Config.js
@@ -1,3 +1,2 @@
-module.exports = {
-    mongodb://nodeforum-shard-00-00.6hlms.mongodb.net:27017,nodeforum-shard-00-01.6hlms.mongodb.net:27017,nodeforum-shard-00-02.6hlms.mongodb.net:27017/MongoDB?replicaSet=atlas-1xsmdf-shard-0" --ssl --authenticationDatabase admin --username audrilinares --password sept92
-}
+module.exports ={
+ mongoUri: "mongodb+srv://audrilinares:sept92@nodeforum.6hlms.mongodb.net/MongoDB?retryWrites=true&w=majority"}
